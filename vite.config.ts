716b2773bd9243618144211f315abbdd590c3514
truongtuan2309 <tranhtuan2309@gmail.com>
2024-12-18 00:06:17 +0700

    optimize

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,34 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from '@svgr/rollup'
 import { visualizer } from 'rollup-plugin-visualizer'
 import viteCompression from 'vite-plugin-compression'
-import MillionLint from "@million/lint";
+import MillionLint from '@million/lint'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr(), visualizer({ open: true }), viteCompression(),MillionLint.vite()],
+  plugins: [react(), tsconfigPaths(), svgr(), visualizer(), viteCompression(), MillionLint.vite()],
   server: {
     port: 3000,
     open: true
   },
+  cacheDir: 'node_modules/.vite',
   build: {
-    sourcemap: true, // Generate source maps for debugging
+    minify: 'terser',
+    cssMinify: 'lightningcss',
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString()
           }
+          if (id.includes('src/pages/')) {
+            return 'pages'
+          }
+          if (id.includes('src/components/')) {
+            return 'components'
+          }
         }
       }
-    }
+    },
+    chunkSizeWarningLimit: 1000
   },
   envPrefix: 'ENV_'
 })
